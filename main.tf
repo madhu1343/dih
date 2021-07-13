191d2b77c065d4f6f50c4990aa435edb884ef41a
madhu1343 <madhavi2038@outlook.com>
2021-07-13 22:10:44 +0530

    commit

--- main.tf
+++ main.tf
@@ -14,28 +14,28 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_application_insights" "example" {
-  name                = "example-appinsights"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  application_type    = "web"
-}
+// resource "azurerm_application_insights" "example" {
+//   name                = "example-appinsights"
+//   location            = azurerm_resource_group.example.location
+//   resource_group_name = azurerm_resource_group.example.name
+//   application_type    = "web"
+// }
 
-resource "azurerm_monitor_action_group" "example" {
-  name                = "example-action-group"
-  resource_group_name = azurerm_resource_group.example.name
-  short_name          = "exampleactiongroup"
-}
+// resource "azurerm_monitor_action_group" "example" {
+//   name                = "example-action-group"
+//   resource_group_name = azurerm_resource_group.example.name
+//   short_name          = "exampleactiongroup"
+// }
 
-resource "azurerm_monitor_smart_detector_alert_rule" "example" {
-  name                = "example-smart-detector-alert-rule"
-  resource_group_name = azurerm_resource_group.example.name
-  severity            = "Sev0"
-  scope_resource_ids  = [azurerm_application_insights.example.id]
-  frequency           = "PT1M"
-  detector_type       = "FailureAnomaliesDetector"
+// resource "azurerm_monitor_smart_detector_alert_rule" "example" {
+//   name                = "example-smart-detector-alert-rule"
+//   resource_group_name = azurerm_resource_group.example.name
+//   severity            = "Sev0"
+//   scope_resource_ids  = [azurerm_application_insights.example.id]
+//   frequency           = "PT1M"
+//   detector_type       = "FailureAnomaliesDetector"
 
-  action_group {
-    ids = [azurerm_monitor_action_group.test.id]
-  }
-}
+//   action_group {
+//     ids = [azurerm_monitor_action_group.test.id]
+//   }
+// }
